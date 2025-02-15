fb493b21e9b570721bd35ad90cd516bb31c4951d
Paulo Martinez <paulomartinez1999@gmail.com>
2025-02-14 23:09:40 -0500

    ejecucion de nmp run build

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div class="love-container">
+      <div v-if="showFireworks" class="fireworks"></div>
+      <v-container class="texto">
+        <v-card class="love-card" elevation="10">
+          <v-card-title class="text-center">Feliz Día del Amor 💖</v-card-title>
+          <v-card-text class="text-center">Eres la persona más especial en mi vida. ¡Te amo! 💕</v-card-text>
+          <v-btn color="pink" dark block @click="triggerFireworks"><i style="font-size: 20px;">Click aquí 😍</i></v-btn>
+        </v-card>
+      </v-container>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      showFireworks: false
+    };
+  },
+  methods: {
+    triggerFireworks() {
+      this.showFireworks = true;
+      setTimeout(() => {
+        this.showFireworks = false;
+      }, 3000);
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.love-container {
+  width: 100%;
+  height: 100vh;
+  background: url('https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2018/02/285357-tarjetas-san-valentin-online-gratis.jpg?tf=1200x') no-repeat center center;
+  background-size: cover;
+  position: relative;
+  overflow: hidden;
+}
+
+.fireworks {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url('../public/img/descarga.jpg');
+  background-size: cover;
+  background-position: center;
+  animation: fadeOut 3s ease-in-out;
+}
+
+@keyframes fadeOut {
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+}
+
+.love-card {
+  max-width: 400px;
+  padding: 20px;
+  background: white;
+  border-radius: 15px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.texto {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
